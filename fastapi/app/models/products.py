84251e32054ab131185bdcc0f897fc0f2332809f
Sho.I1022 <119638592+SHONOSUKE100@users.noreply.github.com>
2024-09-27 08:28:30 +0900

    create register post products api

--- fastapi/app/models/products.py
+++ fastapi/app/models/products.py
@@ -24,14 +24,16 @@ class Product(Base):
     __tablename__ = 'products'
 
     ProductID = Column(Integer, primary_key=True, autoincrement=True)
-    UserID = Column(String(255), ForeignKey('users.uid'), nullable=False)
+    # UserID = Column(String(255), ForeignKey('users.uid'), nullable=False)
+    UserID = Column(String(255), nullable=False)
     Title = Column(String(255), nullable=False)
     Description = Column(Text)
     Price = Column(DECIMAL(10, 2))
     Status = Column(Enum(ProductStatus), nullable=False, default=ProductStatus.DRAFT)
     ImageURL = Column(String(255), nullable=False)
+    demand = Column(Integer, nullable=False)
 
-    user = relationship('User', back_populates='products')
+    # user = relationship('User', back_populates='products')
 
     def __repr__(self):
         return f"<Product(ProductID={self.ProductID}, Title='{self.Title}', Status='{self.Status.name}')>"
